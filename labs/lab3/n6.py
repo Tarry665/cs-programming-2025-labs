8bb3ee8d31b2ebdb839898312b31d25201891887
Пономаренко Тимофей <Tarry@stud.vvsu.ru>
2025-11-14 12:23:35 +1000

    n6.py changed

--- labs/lab3/n6.py
+++ labs/lab3/n6.py
@@ -1,10 +1,20 @@
-fib = int(input("Введите число:"))
+f = int(input("Введите число:"))
+# a = 0
+# b = 1
+# print(a)
+# print(b)
+# for i in range(0, f + 1):
+#     if i == a + b:
+#         a = b
+#         b = i
+#         print(i)
+
+
 a = 0
 b = 1
-print(a)
-print(b)
-for i in range(0,fib + 1):
-    if i == a + b:
-        a = b
-        b = i
-        print(i)
+
+while f > (a + b):
+    c = a
+    a = b
+    b = b + c
+    print (b)
