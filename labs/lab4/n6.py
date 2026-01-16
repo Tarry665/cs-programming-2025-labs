56e53dffcac2b8496bb80775a5f35c8314d3db45
Пономаренко Тимофей <Tarry@stud.vvsu.ru>
2026-01-16 13:24:24 +1000

    lab4 finished

--- labs/lab4/n6.py
+++ labs/lab4/n6.py
@@ -1,9 +1,11 @@
-year = int(input("Введите год: "))
-if year % 4 == 0 and year % 100 != 0:
-    print(year, " - високосный год")
-else:
-    if year % 400 == 0:
-        print(year, "- високосный год")
+try:
+    year = int(input("Введите год: "))
+    if year % 4 == 0 and year % 100 != 0:
+        print(year, " - високосный год")
     else:
-        print(year, "- не високосный год")
-
+        if year % 400 == 0:
+            print(year, "- високосный год")
+        else:
+            print(year, "- не високосный год")
+except ValueError:
+    print("Ошибка, введите число!")
