74a4e4dc2b0c27119ab5a4ebdad4c793b8fde4b3
Пономаренко Тимофей <Tarry@stud.vvsu.ru>
2026-01-23 12:11:38 +1000

    docx fixed

--- labs/lab4/n7.py
+++ labs/lab4/n7.py
@@ -1,9 +1,12 @@
-num = input("Введите три числа: ")
-a, b, c = num.split()
-if a < b and a < c:
-    minimum = a
-elif b < a and b < c:
-    minimum = b
-else:
-    minimum = c
-print(minimum)
+try:
+    num = input("Введите три числа через пробел: ")
+    a, b, c = num.split()
+    if a < b and a < c:
+        minimum = a
+    elif b < a and b < c:
+        minimum = b
+    else:
+        minimum = c
+    print(minimum)
+except ValueError:
+    print("Нужно ввести 3 числа через пробел, роблан!")
